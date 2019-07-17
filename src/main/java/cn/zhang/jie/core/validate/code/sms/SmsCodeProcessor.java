d0d85ffc9345b99ff624b8fd1b7567eee6702d5a
admin <admin@zhangjie>
2019-07-17 22:55:02 +0800

    1

--- src/main/java/cn/zhang/jie/core/validate/code/sms/SmsCodeProcessor.java
+++ src/main/java/cn/zhang/jie/core/validate/code/sms/SmsCodeProcessor.java
@@ -1,5 +1,22 @@
 package cn.zhang.jie.core.validate.code.sms;
 
-public class SmsCodeProcessor {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.context.request.ServletWebRequest;
 
+import cn.zhang.jie.core.validate.code.AbstractValidateCodeProcessor;
+
+@Component("smsValidateCodeProcessor")
+public class SmsCodeProcessor extends AbstractValidateCodeProcessor<SmsCode> {
+
+	@Autowired
+	private SmsCodeSender smsCodeSender;
+	
+	@Override
+	protected void send(ServletWebRequest request, SmsCode code)
+			throws Exception {
+		String mobile = ServletRequestUtils.getRequiredStringParameter(request.getRequest(), "mobile");
+		smsCodeSender.send(mobile, code.getCode());
+	}
 }
