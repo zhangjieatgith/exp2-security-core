4811898910975eb50e745e420456fcb57fccb0d0
admin <admin@zhangjie>
2019-07-21 18:16:57 +0800

    1

---
+++ src/main/java/cn/zhang/jie/core/support/SimpleResponse.java
@@ -0,0 +1,15 @@
+package cn.zhang.jie.core.support;
+
+public class SimpleResponse {
+
+	private Object content;
+	public SimpleResponse(Object content) {
+		this.content = content;
+	}
+	public Object getContent() {
+		return content;
+	}
+	public void setContent(Object content) {
+		this.content = content;
+	}
+}
